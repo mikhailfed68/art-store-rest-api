1c7a50ed205ea8ee7b4ab71997730623c0bf2022
Mikhail Fedorov <104433799+mikhailfed68@users.noreply.github.com>
2023-06-09 21:23:17 +0300

    Update product.py

--- authors/customapi/serializers/product.py
+++ authors/customapi/serializers/product.py
@@ -1,30 +1,22 @@
 from rest_framework import serializers
 from oscarapi.serializers import product
 
-from authors.customapi.services import get_product_price, get_product_availability
+from authors.customapi.services import get_product_price
 
 
 class ProductSerializer(product.ProductSerializer):
     product_class = serializers.StringRelatedField(read_only=True)
     author = serializers.HyperlinkedRelatedField(read_only=True, view_name='painter-detail')
     price = serializers.SerializerMethodField()
-    availability = serializers.SerializerMethodField()
 
     def get_price(self, obj):
         return get_product_price(self, obj)
-
-    def get_availability(self, obj):
-        return get_product_availability(self, obj)
 
 
 class ProductLinkSerializer(product.ProductLinkSerializer):
     product_class = serializers.StringRelatedField(read_only=True)
     author = serializers.StringRelatedField(read_only=True)
     price = serializers.SerializerMethodField()
-    availability = serializers.SerializerMethodField()
 
     def get_price(self, obj):
         return get_product_price(self, obj)
-
-    def get_availability(self, obj):
-        return get_product_availability(self, obj)
