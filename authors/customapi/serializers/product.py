f8981ca17502b62d788bb3c4c95795d0ef7a9bb9
Mikhail <mikhailfedorov1939@gmail.com>
2023-06-11 22:14:07 +0300

    Add original_for_list field for product list

--- authors/customapi/serializers/product.py
+++ authors/customapi/serializers/product.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from oscarapi.serializers import product
+from sorl_thumbnail_serializer.fields import HyperlinkedSorlImageField
 
 from authors.customapi.services import get_product_price
+
+
+class ProductImageListSerializer(product.ProductImageSerializer):
+    """Returns hyperlinked scaled and cached image for Product images."""
+    original_for_list = HyperlinkedSorlImageField('x550',  options={"crop": "center"}, source='original')
 
 
 class ProductSerializer(product.ProductSerializer):
@@ -17,6 +23,7 @@ class ProductLinkSerializer(product.ProductLinkSerializer):
     product_class = serializers.StringRelatedField(read_only=True)
     author = serializers.StringRelatedField(read_only=True)
     price = serializers.SerializerMethodField()
+    images = ProductImageListSerializer(many=True, required=False)
 
     def get_price(self, obj):
         return get_product_price(self, obj)
